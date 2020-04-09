4db321d3487c12982f6a7e8b8eee3789cc54b8fa
Surya Pakalapati <suryarazz@gmail.com>
2020-04-09 00:04:30 -0400

    Ceri Initial Project

---
+++ Ceri/Assets/Scripts/Enemymovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemymovement : MonoBehaviour
+{
+    public float speed;
+
+    private bool move_right = true;
+    public Transform groundDetection;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Edge")
+        {
+            if (move_right == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                move_right = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                move_right = true;
+
+            }
+        }
+    }
+}
---
+++ Ceri/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    public float speed;
+    public float jumpforce;
+    private float moveInput;
+    private Rigidbody2D rb;
+    private bool isTouched;
+    private int jump;
+    public int jumpvalue;
+    public Transform check;
+    public float checkradius;
+    public LayerMask whatisTouched;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jump = jumpvalue;
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+    void Update()
+    {
+        if(isTouched == true)
+        {
+            jump = jumpvalue;
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow) && jump > 0)
+        {
+            rb.velocity = Vector2.up * jumpforce;
+            jump--;
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow) && jump==0 && isTouched == true)
+            {
+            rb.velocity = Vector2.up * jumpforce;
+
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isTouched = Physics2D.OverlapCircle(check.position,checkradius,whatisTouched); 
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Platform")
+        {
+            jump = jumpvalue;
+        }
+        if (collision.gameObject.tag == "Crock")
+        {
+            
+        }
+    }
+}
